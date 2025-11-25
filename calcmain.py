a22a03778108b3bd60ef06044bb548d35f949359
alfeena-km <alfeenamujeeb2020@gmail.com>
2025-11-25 12:06:38 +0530

    Add files via upload

---
+++ calcmain.py
@@ -0,0 +1,7 @@
+from mathoperations.calcfunctions import*
+x=int(input("enter number"))
+y=int(input("enter number"))
+print(addition(x,y))
+print(substraction(x,y))
+print(multiplication(x,y))
+print(division(x,y))
